07f4550a7aaf1d6d9d86b7a8709d38161a9c7481
Rajesh C <rajeshchakali01@gmail.com>
2026-09-11 10:36:08 +0530

    Update EditEducation.jsx

--- Frontend/src/pages/EditEducation.jsx
+++ Frontend/src/pages/EditEducation.jsx
@@ -20,7 +20,7 @@ function EditEducation({ educationId }) {
     const loadEducation = async () => {
       try {
         const response = await fetch(
-          `https://career-canvas-portfolio.onrender.com/Education${educationId}`,
+          `https://career-canvas-portfolio.onrender.com/education${educationId}`,
           {
             credentials: "include",
           }
